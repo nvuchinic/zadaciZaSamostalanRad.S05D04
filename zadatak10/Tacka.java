3c31397e0aa52f48f217164fba37b472e24603e6
nvuchinic <nermin.vucinic@gmail.com>
2014-12-21 18:27:50 +0100

    Update Tacka.java

--- zadatak10/Tacka.java
+++ zadatak10/Tacka.java
@@ -3,6 +3,11 @@ public class Tacka {
 private double x;
 private double y;
 
+/**
+ * konstruktor metod za instanciranje klase, sa parametrima
+ * @param x
+ * @param y
+ */
 public Tacka(double x, double y){
 	this.x=x;
 	this.y=y;
@@ -24,14 +29,22 @@ public void setY(double y) {
 	this.y = y;
 }
 
+/**
+ * metod koj kreira i vraca string reprezentaciju objekta tipa Tacka
+ */
 public String toString(){
 	String tackaToString="";
 	tackaToString="("+x+","+y+")";
 	return tackaToString;
 }
 
+/**
+ * metoda koja mjeri i vraca vrijednost koja predstavlja rastojanje izmedju dva objekta tipa Tacka
+ * @param druga
+ * @return
+ */
 public double rastojanje(Tacka druga){
-	double duz=Math.sqrt(Math.pow((this.x-this.y),2)+Math.pow((druga.x+druga.y),2));
+	double duz=Math.sqrt((Math.pow((this.x-druga.x),2)+Math.pow((this.y-druga.y),2)));
 	return duz;
 }
 
