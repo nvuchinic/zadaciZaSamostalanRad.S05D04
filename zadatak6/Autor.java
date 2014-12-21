af2ed84a19bd21cf8e2a9b043919afea1d2c1332
nvuchinic <nermin.vucinic@gmail.com>
2014-12-21 17:39:02 +0100

    Update Autor.java

--- zadatak6/Autor.java
+++ zadatak6/Autor.java
@@ -3,12 +3,16 @@ private String imePrezime;
 private String email;
 private String spol;
 
+/**
+ * konstruktor metod za klasu, sa parametrima
 public Autor(String imePrezime, String email, String spol){
 	this.imePrezime=imePrezime;
 	this.email=email;
 	this.spol=spol;
 }
 
+/**
+ * konstruktor metod, bez parametara(defaultni)
 public Autor(){
 	this.imePrezime="nekoImePrezime";
 	this.email="nekiEmail";
@@ -38,19 +42,17 @@ public String getSpol(){
 	return spol;
 }
 
-public boolean compare(Autor drugi){
-	boolean isti=false;
-	if(this.imePrezime.equals(drugi.imePrezime)&&this.email.equals(drugi.email)&&this.spol.equals(drugi.spol))
-		isti=true;
-	return isti;
-}
-
+/**
+ * metod koji kreira i vraca String reprezentaciju objekta tipa Autor
 public String toString(){
 	String autorToString="";
 	autorToString="\t\tIme i prezime: "+imePrezime+"\n\t\tEmail: "+email+"\n\t\tSpol: "+spol;
 	return autorToString;
 }
 
+/**
+ * metod koji vraca boolean vrijednost u ovisnosti da li su objekti tipa Autor koji se porede jednaki(jednaki su
+ * ako imaju isto ime, email i spol);
 public boolean equals(Autor drugi){
 	boolean isti=false;
 	if(this.imePrezime.equals(drugi.imePrezime)&&this.email.equals(drugi.email)&&this.spol.equals(drugi.spol))
