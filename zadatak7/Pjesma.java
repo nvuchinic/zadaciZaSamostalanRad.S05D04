0ef72f5428d2d2e6a4186ffb31f1f047826375ce
nvuchinic <nermin.vucinic@gmail.com>
2014-12-21 16:00:12 +0100

    Create Pjesma.java

---
+++ zadatak7/Pjesma.java
@@ -0,0 +1,39 @@
+public class Pjesma {
+private String naziv;
+private double trajanje;
+private Autor autor;
+
+public Pjesma(String naziv, double trajanje, Autor noviAutor){
+	this.naziv=naziv;
+	this.trajanje =trajanje;
+	autor=new Autor();
+	this.autor.setImePrezime(noviAutor.getImePrezime());
+	this.autor.setEmail(noviAutor.getEmail());
+	this.autor.setSpol(noviAutor.getSpol());
+}
+
+public String getNaziv() {
+	return naziv;
+}
+
+public void setNaziv(String naziv) {
+	this.naziv = naziv;
+}
+
+public double getTrajanje() {
+	return trajanje;
+}
+
+public void setTrajanje(double trajanje) {
+	this.trajanje = trajanje;
+}
+
+public String toString(){
+	String pjesmaToString="";
+	pjesmaToString="Naziv: "+naziv+"\nTrajanje: "+trajanje+" minuta"+"\nPodaci o autoru: "+"\n"+autor.toString();
+	return pjesmaToString;
+}
+
+
+
+}
